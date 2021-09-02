55f7fea38ae02ea9d1d11e22d1e344083731362b
Sergei Malykh <sergej.malykh@gmail.com>
2021-09-02 20:34:25 +0200

    JsonTask

---
+++ JsonTask/App.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace JsonTask
+{
+    internal class App
+    {
+        private static void Main()
+        {
+            var request = WebRequest.Create("https://restcountries.eu/rest/v2/region/americas");
+
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            var response = request.GetResponse();
+
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            Console.WriteLine();
+
+            using (var dataStream = response.GetResponseStream())
+            {
+                var responseFromServer = "";
+
+                if (dataStream != null)
+                {
+                    var reader = new StreamReader(dataStream);
+                    responseFromServer = reader.ReadToEnd();
+                }
+
+                var countries = JsonConvert.DeserializeObject<List<Country>>(responseFromServer);
+
+                if (countries != null)
+                {
+                    var sumPopulation = countries.Sum(country => country.Population);
+                    Console.WriteLine($"Total population by country: {sumPopulation}");
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("List of currencies:");
+                Console.WriteLine();
+
+                countries?.ForEach(country => country.Currencies
+                     .ForEach(currency => { Console.WriteLine(currency.Name); }));
+            }
+
+            response.Close();
+        }
+    }
+}
---
+++ JsonTask/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace JsonTask
+{
+    internal class Country
+    {
+        public string Name { get; set; }
+
+        public long Population { get; set; }
+
+        public List<Currency> Currencies { get; set; }
+
+        public class Currency
+        {
+            public string Name { get; set; }
+        }
+    }
+}
