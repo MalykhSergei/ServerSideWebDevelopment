d82566eaf1e6598636680bb7e1ea17f388fbb36d
Sergei Malykh <sergej.malykh@gmail.com>
2021-09-03 23:26:21 +0200

    LogTask

---
+++ LogTask/App.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NLog;
+
+namespace LogTask
+{
+    internal class App
+    {
+        private static void Main()
+        {
+            var appLogger = LogManager.GetCurrentClassLogger();
+
+            appLogger.Info("Programm startet");
+
+            var stringsList = Utils.GetStringsListFromFile("1.txt");
+
+            if (stringsList.Count > 0)
+            {
+                Console.WriteLine($"List of data from file: {string.Join(", ", stringsList)}");
+            }
+
+            appLogger.Info("Programm ended");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LogTask/Utils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NLog;
+
+namespace LogTask
+{
+    internal class Utils
+    {
+        public static List<string> GetStringsListFromFile(string fileName)
+        {
+            var strings = new List<string>();
+
+            var utilsLogger = LogManager.GetCurrentClassLogger();
+
+            try
+            {
+                using var reader = new StreamReader(fileName);
+
+                string currentLine;
+
+                while ((currentLine = reader.ReadLine()) != null)
+                {
+                    strings.Add(currentLine);
+                }
+
+                utilsLogger.Info("The data was successfully read");
+            }
+            catch (FileNotFoundException exception)
+            {
+                utilsLogger.Error(exception, "File is not found");
+                utilsLogger.Info("The data from file was not read");
+            }
+            catch (IOException exception)
+            {
+                utilsLogger.Error(exception, "I/O error");
+                utilsLogger.Info("The data from file was not read");
+            }
+
+            return strings;
+        }
+    }
+}
+
