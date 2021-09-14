0aececaa95776ac4c0248a3976e51ee3aab5bf89
Sergei Malykh <sergej.malykh@gmail.com>
2021-09-14 22:28:16 +0200

    AdoNetTask

---
+++ AdoNetTask/App.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace AdoNetTask
+{
+    internal class App
+    {
+        private static void Main()
+        {
+            try
+            {
+                var reader = Utils.Select("Products");
+                Console.WriteLine($"Total number of products: {reader}");
+                Console.WriteLine();
+
+                Console.WriteLine("Please enter name of product: ");
+                var productName = Console.ReadLine();
+
+                Console.WriteLine("Please enter price of product: ");
+                var productPrice = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter category of product: ");
+                var productCategory = Convert.ToInt32(Console.ReadLine());
+
+                Utils.InsertProduct(productName, productPrice, productCategory);
+                Console.WriteLine();
+
+                Console.WriteLine("Please enter new product name: ");
+                productName = Console.ReadLine();
+
+                Console.WriteLine("Enter the ID of the product whose name you want to change: ");
+                var productId = Convert.ToInt32(Console.ReadLine());
+
+                Utils.UpdateProduct(productName, productId);
+                Console.WriteLine();
+
+                Console.WriteLine("Please enter the name of the product you want to delete: ");
+                productName = Console.ReadLine();
+
+                Utils.DeleteProduct(productName);
+
+
+                var productsWithCategoriesList = Utils.GetProductsAndCategoriesList();
+
+                foreach (var item in productsWithCategoriesList)
+                {
+                    Console.WriteLine(item);
+                }
+
+                var dataSet = Utils.GetProductsAndCategoriesWithSqlDataAdapterList();
+
+                foreach (DataTable dt in dataSet.Tables)
+                {
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        Console.Write("{0}", column.ColumnName);
+                    }
+
+                    Console.WriteLine();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        var cells = row.ItemArray;
+                        foreach (var cell in cells)
+                        {
+                            Console.Write("{0} ", cell);
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                Console.WriteLine("Invalid name of database");
+            }
+        }
+    }
+}
---
+++ AdoNetTask/Utils.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace AdoNetTask
+{
+    internal class Utils
+    {
+        private const string ConnectionString = @"Server=.;Initial Catalog=Shop;Integrated Security=true;";
+
+        private static SqlConnection GetConnection()
+        {
+            var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            return connection;
+        }
+
+        private static SqlCommand GetSqlCommand(string sql)
+        {
+            using var command = new SqlCommand(sql, GetConnection());
+
+            return command;
+        }
+
+        public static object Select(string databaseName)
+        {
+            var sql = $"SELECT COUNT(*) FROM dbo.{databaseName}";
+
+            return (int)GetSqlCommand(sql).ExecuteScalar();
+        }
+
+        public static void InsertCategory(string categoryName)
+        {
+            const string sql = "INSERT INTO dbo.Categories(name) VALUES(@categoryName)";
+
+            var command = GetSqlCommand(sql);
+            command.Parameters.Add(new SqlParameter("@categoryName", SqlDbType.NVarChar)
+            {
+                Value = categoryName
+            });
+
+            command.ExecuteNonQuery();
+        }
+
+        public static void InsertProduct(string productName, int productPrice, int productCategory)
+        {
+            const string sql = "INSERT INTO dbo.Products(name, price, categoryId)" +
+                               "VALUES(@productName, @productPrice, @productCategory)";
+
+            var command = GetSqlCommand(sql);
+
+            var sqlParameters = new[]
+            {
+                new SqlParameter("@productName", SqlDbType.NVarChar)
+                {
+                    Value = productName
+                },
+                new SqlParameter("@productPrice", SqlDbType.Int)
+                {
+                    Value = productPrice
+                },
+                new SqlParameter("@productCategory", SqlDbType.Int)
+                {
+                    Value = productCategory
+                }
+            };
+
+            command.Parameters.AddRange(sqlParameters);
+            command.ExecuteNonQuery();
+        }
+
+        public static void UpdateProduct(string newProductName, int id)
+        {
+            const string sql = "UPDATE dbo.Products " +
+                               "SET NAME= @newProductName " +
+                               "WHERE ID= @id";
+
+            using var command = GetSqlCommand(sql);
+
+            var sqlParameters = new[]
+            {
+                new SqlParameter("@newProductName", SqlDbType.NVarChar)
+                {
+                    Value = newProductName
+                },
+                new SqlParameter("@id", SqlDbType.Int)
+                {
+                    Value = id
+                }
+            };
+
+            command.Parameters.AddRange(sqlParameters);
+            command.ExecuteNonQuery();
+        }
+
+        public static void DeleteProduct(string productName)
+        {
+            const string sql = "DELETE FROM dbo.Products " +
+                               "WHERE NAME= @productName";
+
+            using var command = GetSqlCommand(sql);
+
+            command.Parameters.Add(new SqlParameter("@productName", SqlDbType.NVarChar)
+            {
+                Value = productName
+            });
+
+            command.ExecuteNonQuery();
+        }
+
+        public static List<object> GetProductsAndCategoriesList()
+        {
+            var productsWithCategoriesList = new List<object>();
+
+            const string sql = "SELECT pr.NAME AS Products_name, cat.NAME AS Categories_Name " +
+                               "FROM[dbo].Products AS pr " +
+                               "LEFT JOIN[dbo].Categories AS cat " +
+                               "ON pr.CATEGORYID = cat.ID";
+
+            using var reader = GetSqlCommand(sql).ExecuteReader();
+
+            while (reader.Read())
+            {
+                productsWithCategoriesList.Add($"{reader.GetValue(0)} {reader.GetValue(1)}");
+            }
+
+            return productsWithCategoriesList;
+        }
+
+        public static DataSet GetProductsAndCategoriesWithSqlDataAdapterList()
+        {
+            const string sql = "SELECT pr.NAME AS Products_name, cat.NAME AS Categories_Name " +
+                               "FROM[dbo].Products AS pr " +
+                               "LEFT JOIN[dbo].Categories AS cat " +
+                               "ON pr.CATEGORYID = cat.ID";
+
+            using var adapter = new SqlDataAdapter(sql, GetConnection());
+
+            var dataSet = new DataSet();
+
+            adapter.Fill(dataSet);
+
+            return dataSet;
+        }
+    }
+}
